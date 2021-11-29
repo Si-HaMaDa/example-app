b593cec974972b721065626bc0f134e44187f2bf
Si HaMaDa <nrookman_27@yahoo.com>
2021-11-29 21:01:15 +0200

    init commit

---
+++ resources/views/articals.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Start</title>
+</head>
+<body>
+    <h1>Cooming soon</h1>
+    <h2>Articals</h2>
+</body>
+</html>
